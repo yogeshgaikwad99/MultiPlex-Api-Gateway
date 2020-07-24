f9e99df8a39981ff0c078b5444ef87fb98675acb
yogeshgaikwad410 <yogeshgaikwad410@gmail.com>
2020-07-24 13:15:28 +0530

    first Api Gateway

---
+++ Multiplex-Api-Gateway/src/main/java/com/app/lti/MultiplexApiGatewayApplication.java
@@ -0,0 +1,43 @@
+package com.app.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class MultiplexApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplexApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+    @Primary
+    public CorsFilter corsFilter() {
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+	
+
+}
